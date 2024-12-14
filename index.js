9a5b72ec9f4646a111369b3140260f81531b8690
E. Wright <ewright1585@gmail.com>
2024-12-14 15:55:13 -0500

    updated navigation

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('index.js is running');
+
+let headerNav = document.querySelector("#headerNav");
+let headerNavLinks = document.querySelectorAll("#headerNav li a");
+
+headerNavLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+        headerNav.classList.remove("show");
+    });
+});
